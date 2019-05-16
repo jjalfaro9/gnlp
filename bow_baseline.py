e457abb0e553da16766cffe76b0147ad101c31ef
Josue Alfaro <jjalfaro@utexas.edu>
2019-05-16 14:32:29 -0500

    save results.json file that gives us image retreval info

--- bow_baseline.py
+++ bow_baseline.py
@@ -10,6 +10,7 @@
 # so you can save them and manually verify what happened
 import numpy as np
 import argparse
+import json 
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
@@ -36,6 +37,7 @@ def get_full_corpus(metadata_path, PNG_DIR):
 def split_corpus_test_train(data):
     descriptions = data['description'].to_numpy()
     icons = data['icon'].to_numpy()
+    print("Len of descriptions (%s) and icons (%s) should be the same!" % (len(descriptions), len(icons)))
     print("Example of icon (icon livin' lol) %s " % icons[0])
     return train_test_split(descriptions, icons, train_size=0.6)
 
@@ -63,7 +65,6 @@ def do_some_stuff(meta, pngs):
 
     # features are in the same space, computed with the same vgg16 model
     print("Let's get some img features!")
-    print("you're only getting a small set of img features, to figure things out *******!!")
     training_icon_f, testing_icon_f = get_features(pngs, i_train, i_test)
     img_n = NearestNeighbors(metric='cosine')
     # we need to only look up unseen img data!
@@ -74,25 +75,36 @@ def do_some_stuff(meta, pngs):
     # accuracy is calculated by seeing if the k-closest images contain my golden image!
     accuracy = 0
     counter = 0
+    img_map = dict()
     for i, r in enumerate(results):
         neighbors_f = [training_icon_f[j] for j in r]
         counter += 1
 
         img_neighbors = img_n.kneighbors(neighbors_f, return_distance=False)
+        # okay, okay! so r has a len based on how many k neighbors we want, so for each of those we're finding j neighbors of the images
+        # so img_neighbors is a k x j matrix!
         # okay! this contains the nearest neighbors [[], [], [], [], []] and within that are indecies
+        d = {}
         closests_featured_imgs = [[testing_icon_f[n] for n in neighbor]for neighbor in img_neighbors]
+        d['close_imgs'] = [ i_test[close_img] for neighbor in img_neighbors for close_img in neighbor ]
         if np.array(testing_icon_f[i]) in np.array(closests_featured_imgs):
             accuracy += 1
+            d['golden_present'] = True
+        else:
+            d['golden_present'] = False
         if (counter % 5000 == 0):
             print("Counter: %s" % counter)
             print("Current accuracy: %s" % (accuracy/len(testing_icon_f)))
-    t_l = len(testing_icon_f)
-    accuracy = accuracy / t_l
+        img_map[i_test[i]] = d
+    testing_len = len(testing_icon_f)
+    train_len = len(training_icon_f)
+    accuracy = accuracy / testing_len
     print("Accuracy for finding an image %f" % accuracy)
     with open('results.txt', 'w') as f:
-        f.write(f'Accuracy for finding an image with bag of words baseline model: => {accuracy}')
-        f.write(f'size of testing data: => {t_l}')
-    
+        f.write(f'Accuracy for finding an image with bag of words baseline model: => {accuracy}\n')
+        f.write(f'size of testing data: => {testing_len}, size of training data: => {train_len}')
+    with open('results.json', 'w') as r:
+        json.dump(img_map, r)
 
 
 
